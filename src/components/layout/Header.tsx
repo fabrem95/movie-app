d01858d7f6cbc73fbf6cf0d6a59d417c2700ed5e
Fabrem <fjbremermann@gmail.com>
2023-01-22 23:51:59 -0300

    Header logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,35 @@
-import { Header as MantineHeader } from "@mantine/core";
+import {
+	Box,
+	createStyles,
+	Header as MantineHeader,
+	Image,
+	Text,
+} from "@mantine/core";
+
+import film_png from "../../assets/imgs/film.png";
 
 type Props = {};
 
+const useStyles = createStyles((theme) => ({
+	logo: {
+		width: "5rem",
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+	},
+}));
+
 export const Header = (props: Props) => {
+	const { classes } = useStyles();
+
 	return (
-		<MantineHeader height={60} p="xs">
-			{/* Header content */}
+		<MantineHeader height={"4rem"} p="0.4rem">
+			<Box className={classes.logo}>
+				<Image src={film_png} width={30} />
+				<Text ta="center" c="teal" weight={600}>
+					Movie App
+				</Text>
+			</Box>
 		</MantineHeader>
 	);
 };
